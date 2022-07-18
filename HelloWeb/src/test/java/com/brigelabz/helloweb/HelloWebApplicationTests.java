7cac3a0ed85c15ae0c8e36cc4c0a120d4fb3eb90
nehachougule1998 <nchougule734@gmail.com>
2022-07-18 09:49:51 +0530

    Spring app developement

---
+++ HelloWeb/src/test/java/com/brigelabz/helloweb/HelloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brigelabz.helloweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
